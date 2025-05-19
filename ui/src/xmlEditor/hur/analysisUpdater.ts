edab97d04d1bc1dacdb837535220c7445a51a2e5
Björn Eyselein <eyselein@infosim.net>
2025-05-19 14:38:33 +0200

    Merge #7

--- ui/src/xmlEditor/hur/analysisUpdater.ts
+++ ui/src/xmlEditor/hur/analysisUpdater.ts
@@ -1,49 +1,46 @@
-import {MorphologicalAnalysis} from '../../model/morphologicalAnalysis';
-import {SelectableLetteredAnalysisOption} from '../../model/analysisOptions';
-import {analyze} from './analyze';
-import {getGrammaticalMorphemes} from './splitter';
+import { MorphologicalAnalysis } from '../../model/morphologicalAnalysis';
+import { analyze } from './analyze';
+import { getGrammaticalMorphemes } from './splitter';
+import { Spec } from 'immutability-helper';
+
+// 97
+const codePointLowerA: number = 'a'.codePointAt(0) || 97;
 
 // Falls der Benutzer eine neue Segmentierung eingegeben hat, muss diese neu analysiert werden.
-export function updateHurrianAnalysis(oldma: MorphologicalAnalysis, referenceWord: string)
-{
-	const grammaticalMorphemes = getGrammaticalMorphemes(referenceWord);
-	const tags: string[] | null = analyze(grammaticalMorphemes, oldma.paradigmClass);
-	if (tags !== null && tags.length > 0)
-	{
-		if (tags.length == 1)
-		{
-			return {referenceWord: {$set: referenceWord},
-					analysis: {$set: tags[0]},
-					encliticsAnalysis: {$set: undefined},
-					selected: {$set: false}
-			};
-		}
-		else
-		{
-			const analysisOptions: SelectableLetteredAnalysisOption[] = [];
-			let c = 97;
-			for (const tag of tags)
-			{
-				const analysisOption: SelectableLetteredAnalysisOption = 
-				{
-					letter: String.fromCodePoint(c),
-					analysis: tag,
-					selected: false
-				};
-				c++;
-				analysisOptions.push(analysisOption);
-			}
-			return {referenceWord: {$set: referenceWord},
-					analysis: {$set: analysisOptions[0].analysis},
-					encliticsAnalysis: {$set: undefined}
-			};
-		}
-	}
-	else
-	{
-		return {referenceWord: {$set: referenceWord},
-				encliticsAnalysis: {$set: undefined},
-				selected: {$set: false}
-		};
-	}
+export function updateHurrianAnalysis(oldma: MorphologicalAnalysis, referenceWord: string): Spec<MorphologicalAnalysis> {
+  const grammaticalMorphemes = getGrammaticalMorphemes(referenceWord);
+
+  const tags: string[] | null = analyze(grammaticalMorphemes, oldma.paradigmClass);
+
+  // no tags
+  if (tags === null || tags.length === 0) {
+    return {
+      referenceWord: { $set: referenceWord },
+      encliticsAnalysis: { $set: undefined },
+      selected: { $set: false }
+    };
+  }
+
+  // single tag
+  if (tags.length === 1) {
+    return {
+      referenceWord: { $set: referenceWord },
+      analysis: { $set: tags[0] },
+      encliticsAnalysis: { $set: undefined },
+      selected: { $set: false }
+    };
+  }
+
+  // multiple tags
+  return {
+    referenceWord: { $set: referenceWord },
+    analysisOptions: {
+      $set: tags.map((tag, index) => ({
+        letter: String.fromCodePoint(codePointLowerA + index),
+        analysis: tag,
+        selected: false
+      }))
+    },
+    selected: { $set: false }
+  };
 }
--- ui/src/xmlEditor/hur/analyze.ts
+++ ui/src/xmlEditor/hur/analyze.ts
@@ -1,146 +1,109 @@
-// Dieses Modul kann Segmentierungen morphologische Glossierung zuweisen.
-// Sowohl den vom Benutzer eingegebenen Segmentierungen als auch den automatisch erstellten.
-// Die Grammatik (Annotationen von Suffixen und ihre Reihenfolge) wird als 2 JSON-Dateien in "public" gespeichert.
-function loadFile(filePath: string): string
-{
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('GET', filePath, false);
-    xmlhttp.send();
-    if (xmlhttp.status == 200) 
-	{
-        const result: string = xmlhttp.responseText;
-		return result;
-    }
-	else
-	{
-		throw new Error('File not found: ' + filePath);
-	}
-}
+import { hurrianSuffixPositions, hurrianEncliticSuffixPositions } from './hurrianSuffixPositions';
+import { hurrianMorphemes as grammemesByPartOfSpeech } from './morphemes';
 
 type Seq = string | string[];
 
-const grammemesByPartOfSpeech: {[key: string]: {[key: string]: {[key: string]: Seq}}}
-	= JSON.parse(loadFile(process.env.PUBLIC_URL + '/hurrian/morphemes.json'));
-const suffixPositions: {[key: string]: string[]}
-	= JSON.parse(loadFile(process.env.PUBLIC_URL + '/hurrian/positions.json'));
-const encliticPositions: string[] = suffixPositions.enclitic;
-delete suffixPositions.enclitic;
+function makeCategoryGroup(partOfSpeech: string, position: string, sep: string): string {
+  const category: string = position.replace('2', '');
 
-function makeCategoryGroup(partOfSpeech: string, position: string, sep: string): string
-{
-    const category: string = position.replace('2', '');
-	const grammemes: {[key: string]: {[key: string]: Seq}} = grammemesByPartOfSpeech[partOfSpeech];
-    if (grammemes[category] === undefined)
-	{
-        console.log(category);
-    }
-    const values: string = Object.keys(grammemes[category]).join('|');
-    const group: string = '(?:' + sep + '(?<' + position + '>'+ values + '))?';
-    return group;
+  const grammemes: { [key: string]: { [key: string]: Seq } } = grammemesByPartOfSpeech[partOfSpeech];
+
+  if (grammemes[category] === undefined) {
+    console.log(category);
+  }
+
+  const values: string = Object.keys(grammemes[category]).join('|');
+
+  return '(?:' + sep + '(?<' + position + '>' + values + '))?';
 }
 
-export const partsOfSpeech: string[] = Array.from(Object.keys(grammemesByPartOfSpeech))
-	.filter((partOfSpeech: string) => partOfSpeech != 'enclitic');
-const enclitics: string = encliticPositions
-	.map((category: string) => makeCategoryGroup('enclitic', category, '='))
-	.join('');
-const expr: {[key: string]: RegExp} = {};
+export const partsOfSpeech: string[] = Object.keys(grammemesByPartOfSpeech)
+  .filter((partOfSpeech) => partOfSpeech !== 'enclitic');
 
-for (const pos of partsOfSpeech)
-{
-	const suffixes: string = suffixPositions[pos]
-		.map((category: string) => makeCategoryGroup(pos, category, '-'))
-		.join('');
-	const segmentedGrammaticalMorphemes: string = suffixes + enclitics + '$';
-	expr[pos] = new RegExp(segmentedGrammaticalMorphemes);
+const enclitics: string = hurrianEncliticSuffixPositions
+  .map((category: string) => makeCategoryGroup('enclitic', category, '='))
+  .join('');
+
+const expr: { [key: string]: RegExp } = {};
+
+for (const pos of partsOfSpeech) {
+  const suffixes: string = hurrianSuffixPositions[pos]
+    .map((category: string) => makeCategoryGroup(pos, category, '-'))
+    .join('');
+
+  const segmentedGrammaticalMorphemes: string = suffixes + enclitics + '$';
+
+  expr[pos] = new RegExp(segmentedGrammaticalMorphemes);
 }
 
-function expand(array: Seq[]): string[][]
-{
-	let sequences: string[][] = [[]];
-	for (let i = 0; i < array.length; i++)
-	{
-		const element = array[i];
-		if (typeof(element) === 'string')
-		{
-			for (const sequence of sequences)
-			{
-				sequence.push(element);
-			}
-		}
-		else
-		{
-			const newSequences: string[][] = [];
-			for (const sequence of sequences)
-			{
-				for (const subelement of element)
-				{
-					newSequences.push(sequence.concat([subelement]));
-				}
-			}
-			sequences = newSequences;
-		}
-	}
-	return sequences;
-}
+function expand(array: Seq[]): string[][] {
+  let sequences: string[][] = [[]];
 
-export function analyze(grammaticalMorphemes: string, pos: string): string[] | null
-{
-    if (pos == 'indecl')
-	{
-        return null;
-    }
-    else 
-	{
-        const match = grammaticalMorphemes.match(expr[pos]);
-        if (match === null)
-		{
-            return null;
+  for (const element of array) {
+    if (typeof (element) === 'string') {
+      for (const sequence of sequences) {
+        sequence.push(element);
+      }
+    } else {
+      const newSequences: string[][] = [];
+      for (const sequence of sequences) {
+        for (const subelement of element) {
+          newSequences.push(sequence.concat([subelement]));
         }
-        else
-		{
-            const groups = match.groups;
-			if (groups !== undefined)
-			{
-				const suffixChains: string[][] = 
-				expand(
-					suffixPositions[pos]
-						.filter(position => groups[position] !== undefined)
-						.map(position =>
-						{
-							const grammemes: {[key: string]: Seq} =
-								grammemesByPartOfSpeech[pos][position.replace('2', '')];
-							return grammemes[groups[position]];
-						})
-				);
-				const encliticChains: string[][] =
-				expand(
-					encliticPositions
-						.filter(position => groups[position] !== undefined)
-						.map(position => 
-						{
-							const grammemes: {[key: string]: Seq} =
-								grammemesByPartOfSpeech.enclitic[position.replace('2', '')];
-							return grammemes[groups[position]];
-						})
-				);
-				const result: string[] = [];
-				for (const suffixChain of suffixChains)
-				{
-					for (const encliticChain of encliticChains)
-					{
-						const chain: string = encliticChain.length > 0 ?
-							suffixChain.join('-') + '=' + encliticChain.join('=') :
-							suffixChain.join('-');
-						result.push(chain);
-					}
-				}
-				return result;
-			}
-			else
-			{
-				return null;
-			}
-        }
+      }
+      sequences = newSequences;
     }
+  }
+
+  return sequences;
+}
+
+/**
+ * Dieses Modul kann Segmentierungen morphologische Glossierung zuweisen.
+ * Sowohl den vom Benutzer eingegebenen Segmentierungen als auch den automatisch erstellten.
+ * Die Grammatik (Annotationen von Suffixen und ihre Reihenfolge) wird als 2 JSON-Dateien in "public" gespeichert.
+ */
+export function analyze(grammaticalMorphemes: string, pos: string): string[] | null {
+  if (pos == 'indecl') {
+    return null;
+  }
+
+  const match = grammaticalMorphemes.match(expr[pos]);
+  if (match === null) {
+    return null;
+  }
+
+  const groups = match.groups;
+  if (groups === undefined) {
+    return null;
+  }
+
+  const suffixChains: string[][] = expand(
+    hurrianSuffixPositions[pos]
+      .filter(position => groups[position] !== undefined)
+      .map(position => {
+        const grammemes: { [key: string]: Seq } = grammemesByPartOfSpeech[pos][position.replace('2', '')];
+        return grammemes[groups[position]];
+      })
+  );
+
+  const encliticChains: string[][] = expand(
+    hurrianEncliticSuffixPositions
+      .filter(position => groups[position] !== undefined)
+      .map(position => {
+        const grammemes: { [key: string]: Seq } = grammemesByPartOfSpeech.enclitic[position.replace('2', '')];
+        return grammemes[groups[position]];
+      })
+  );
+
+  const result: string[] = [];
+  for (const suffixChain of suffixChains) {
+    for (const encliticChain of encliticChains) {
+      const chain: string = encliticChain.length > 0 ?
+        suffixChain.join('-') + '=' + encliticChain.join('=') :
+        suffixChain.join('-');
+      result.push(chain);
+    }
+  }
+  return result;
 }
