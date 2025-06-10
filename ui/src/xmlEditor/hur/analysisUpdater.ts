ded4fcf685afd1e254cb52979964089698573bfe
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-06-10 13:12:03 +0300

    Automatic gloss update by segmentation edit

--- ui/src/xmlEditor/hur/analysisUpdater.ts
+++ ui/src/xmlEditor/hur/analysisUpdater.ts
@@ -2,6 +2,9 @@ import { MorphologicalAnalysis } from '../../model/morphologicalAnalysis';
 import { analyze } from './analyze';
 import { getGrammaticalMorphemes } from './splitter';
 import { Spec } from 'immutability-helper';
+import { getStem } from './splitter';
+import { getPos } from './glossUpdater';
+import { retrieveGloss } from './glossProvider';
 
 // 97
 const codePointLowerA: number = 'a'.codePointAt(0) || 97;
@@ -12,35 +15,79 @@ export function updateHurrianAnalysis(oldma: MorphologicalAnalysis, referenceWor
 
   const tags: string[] | null = analyze(grammaticalMorphemes, oldma.paradigmClass);
 
+  const stem = getStem(referenceWord);
+  const pos = getPos(oldma.paradigmClass);
+  const glosses: Set<string> | null = retrieveGloss(stem, pos);
+  let newTranslation: string | null;
+  if (glosses === null) {
+    newTranslation = null;
+  } else {
+    newTranslation = Array.from(glosses).sort().join('; ');
+  }
+
   // no tags
   if (tags === null || tags.length === 0) {
-    return {
-      referenceWord: { $set: referenceWord },
-      encliticsAnalysis: { $set: undefined },
-      selected: { $set: false }
-    };
+    if (newTranslation === null) {
+      return {
+        referenceWord: { $set: referenceWord },
+        encliticsAnalysis: { $set: undefined },
+        selected: { $set: false }
+      };
+    } else {
+      return {
+        referenceWord: { $set: referenceWord },
+        encliticsAnalysis: { $set: undefined },
+        selected: { $set: false },
+        translation: {$set: newTranslation}
+      };
+    }
   }
 
   // single tag
   if (tags.length === 1) {
-    return {
-      referenceWord: { $set: referenceWord },
-      analysis: { $set: tags[0] },
-      encliticsAnalysis: { $set: undefined },
-      selected: { $set: false }
-    };
+    if (newTranslation === null) {
+      return {
+        referenceWord: { $set: referenceWord },
+        analysis: { $set: tags[0] },
+        encliticsAnalysis: { $set: undefined },
+        selected: { $set: false }
+      };
+    } else {
+      return {
+        referenceWord: { $set: referenceWord },
+        analysis: { $set: tags[0] },
+        encliticsAnalysis: { $set: undefined },
+        selected: { $set: false },
+        translation: {$set: newTranslation}
+      };
+    }
   }
 
   // multiple tags
-  return {
-    referenceWord: { $set: referenceWord },
-    analysisOptions: {
-      $set: tags.map((tag, index) => ({
-        letter: String.fromCodePoint(codePointLowerA + index),
-        analysis: tag,
-        selected: false
-      }))
-    },
-    selected: { $set: false }
-  };
+  if (newTranslation === null) {
+    return {
+      referenceWord: { $set: referenceWord },
+      analysisOptions: {
+        $set: tags.map((tag, index) => ({
+          letter: String.fromCodePoint(codePointLowerA + index),
+          analysis: tag,
+          selected: false
+        }))
+      },
+      selected: { $set: false }
+    };
+  } else {
+    return {
+      referenceWord: { $set: referenceWord },
+      analysisOptions: {
+        $set: tags.map((tag, index) => ({
+          letter: String.fromCodePoint(codePointLowerA + index),
+                                        analysis: tag,
+                                        selected: false
+        }))
+      },
+      selected: { $set: false },
+      translation: {$set: newTranslation}
+    };
+  }
 }
