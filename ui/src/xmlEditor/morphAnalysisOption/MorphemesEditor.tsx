de6eaf9ecacd83de27397fa35d92610a439a21f8
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-06-16 09:09:02 +0300

    Treat fragmentary morphemes specially

--- ui/src/xmlEditor/morphAnalysisOption/MorphemesEditor.tsx
+++ ui/src/xmlEditor/morphAnalysisOption/MorphemesEditor.tsx
@@ -25,7 +25,9 @@ const kindToBoundary: { [key: string]: string } = {
   'stem': '',
   'zero': '.',
   'suffix': '-',
-  'enclitic': '='
+  'enclitic': '=',
+  'fragment': '-',
+  'stemFragment': ''
 };
 
 const boundaryToKind: { [key: string]: string } = {
@@ -51,6 +53,9 @@ class Morpheme {
     return kindToBoundary[this.kind] + this.form;
   }
   getTag(position: number): string {
+    if (this.kind === 'fragment') {
+      return '';
+    }
     return this.getTagBoundary(position) + this.tag;
   }
   getTagBoundary(position: number): string {
@@ -69,32 +74,65 @@ function makeAnalysis(morphemes: Morpheme[]): string {
   return morphemes.slice(1).map((morpheme, i) => morpheme.getTag(i)).join('');
 }
 
+function formIsFragment(form: string): boolean {
+  return form.includes('[') || form.includes(']');
+}
+
+function kindIsFragment(kind: string): boolean {
+  return kind === 'fragment' || kind === 'stemFragment';
+}
+
+function morphemeIsFragment(morpheme: Morpheme): boolean {
+  return kindIsFragment(morpheme.kind);
+}
+
+function removeSuffix(suffix: string, s: string) {
+  while(s.endsWith(suffix)) {
+    s = s.substring(0, s.length - suffix.length);
+  }
+  return s;
+}
+
 function buildMorphemes(segmentation: string, translation: string, analysis: string): Morpheme[] {
   const forms: [string, string][] = split(segmentation);
+  analysis = removeSuffix('-', analysis);
   const tags: [string, string][] = analysis === '' ? []
     : split(analysis);
   const morphemes: Morpheme[] = [];
   let j = tags.length - 1;
   for (let i = forms.length - 1; i >= 0; i--) {
     const [form, boundary] = forms[i];
-    let tag: string;
-    while (j >= 0 && tags[j][1] === '.') {
-      tag = tags[j][0];
-      const zero = new Morpheme('', tag, 'zero');
-      morphemes.push(zero);
+    if (formIsFragment(form)) {
+      let kind, tag: string;
+      if (i === 0) {
+        kind = 'stemFragment';
+        tag = 'u.B.';
+      } else {
+        kind = 'fragment';
+        tag = '<fragm>';
+      }
+      const morpheme = new Morpheme(form, tag, kind);
+      morphemes.push(morpheme);
+    } else {
+      let tag: string;
+      while (j >= 0 && tags[j][1] === '.') {
+        tag = tags[j][0];
+        const zero = new Morpheme('', tag, 'zero');
+        morphemes.push(zero);
+        j--;
+      }
+      const kind: string = boundaryToKind[boundary];
+      if (i === 0) {
+        tag = translation;
+      } else if (j >= 0) {
+        tag = tags[j][0];
+      } else {
+        tag = '';
+      }
+      const morpheme = new Morpheme(form, tag, kind);
+      morphemes.push(morpheme);
       j--;
     }
-    const kind: string = boundaryToKind[boundary];
-    if (i === 0) {
-      tag = translation;
-    } else if (j >= 0) {
-      tag = tags[j][0];
-    } else {
-      tag = '';
-    }
-    const morpheme = new Morpheme(form, tag, kind);
-    morphemes.push(morpheme);
-    j--;
   }
   return morphemes.reverse();
 }
@@ -113,7 +151,7 @@ export function MorphemesEditor({
           <div className="field-box">
             <select
               className="morpheme-input"
-              defaultValue={morpheme.kind}
+              value={morpheme.kind}
               onChange={(event) => {
                 morphemes[i].kind = event.target.value;
                 onSegmentationChange(makeSegmentation(morphemes));
@@ -123,6 +161,8 @@ export function MorphemesEditor({
               <option value='suffix'>Suffix</option>
               <option value='enclitic'>Enklitik</option>
               <option value='zero'>Nullsuf.</option>
+              <option value='fragment'>Fragm.</option>
+              <option value='stemFragment'>St.-fr.</option>
             </select>
           </div>
           <div className="field-box">
@@ -137,23 +177,25 @@ export function MorphemesEditor({
             >
             </input>
           </div>
-          <div className="field-box">
-            <input
-              type="text"
-              className="morpheme-input"
-              defaultValue={morpheme.tag}
-              onChange={(event) => {
-                morphemes[i].tag = event.target.value;
-                if (i == 0) {
-                  onTranslationChange(event.target.value);
-                }
-                else {
-                  onAnalysisChange(makeAnalysis(morphemes));
-                }
-              }}
-            >
-            </input>
-          </div>
+          {!(morphemeIsFragment(morpheme)) &&
+            <div className="field-box">
+              <input
+                type="text"
+                className="morpheme-input"
+                defaultValue={morpheme.tag}
+                onChange={(event) => {
+                  morphemes[i].tag = event.target.value;
+                  if (i == 0) {
+                    onTranslationChange(event.target.value);
+                  }
+                  else {
+                    onAnalysisChange(makeAnalysis(morphemes));
+                  }
+                }}
+              >
+              </input>
+            </div>
+          }
         </div>
       );
     })}
