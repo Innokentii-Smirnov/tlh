d24a1d444f401ad8f3c4c9e31280d00f3e1e3db3
Innokentii Smirnov <innokenty.smirnof@gmail.com>
2025-07-12 12:02:11 +0300

    Remove unused module

--- ui/src/xmlEditor/hur/dict/valueCounter.ts
+++
@@ -1,9 +0,0 @@
-import { dictionary } from '../dict/dictionary';
-
-export function countValues(): number {
-  let count = 0;
-  for (const analyses of dictionary.values()) {
-    count += analyses.size;
-  }
-  return count;
-}
