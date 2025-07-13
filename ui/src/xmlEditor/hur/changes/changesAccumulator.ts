013445439eefb30e51479cd2e863156a17d52b23
Innokentii Smirnov <innokenty.smirnof@gmail.com>
2025-07-13 20:23:42 +0300

    Correct and document changes accumulator

--- ui/src/xmlEditor/hur/changes/changesAccumulator.ts
+++ ui/src/xmlEditor/hur/changes/changesAccumulator.ts
@@ -5,28 +5,35 @@ import { writeMorphAnalysisValue } from '../../../model/morphologicalAnalysis';
 const changes = new Map<string, string>();
 const sources = new Map<string, Set<string>>();
 
-export function addChange(source: string, target: string, targetIsExtant: boolean): void {
+export function addChange(origin: string, target: string, targetIsExtant: boolean): void {
+  // We will now have to store the sources of the target
+  let targetSources = sources.get(target);
+  if (targetSources === undefined) {
+    targetSources = new Set<string>();
+    sources.set(target, targetSources);
+  }
+  // An existing target is the source of itself
+  if (targetIsExtant) {
+    targetSources.add(target);
+  }
   // Look up what the original analysis could be derived from
-  const initialSources = sources.get(source);
-  if (initialSources === undefined) {
-    changes.set(source, target);
-    const sourcesSet = new Set<string>();
-    sourcesSet.add(source);
-    if (targetIsExtant) {
-      sourcesSet.add(target);
-    }
-    sources.set(target, sourcesSet);
+  const originSources = sources.get(origin);
+  if (originSources === undefined) {
+    // The original analysis is underived
+    changes.set(origin, target);
+    // The target acquires an additional source
+    targetSources.add(origin);
   } else {
-    // The original analysis should no longer be mapped to its own origins because it has been replaced
-    sources.delete(source);
-    if (targetIsExtant) {
-      initialSources.add(target);
-    }
-    // Save sources for the target
-    sources.set(target, initialSources);
-    // The original analyses of the given analysis and the given analysis itself are mapped to target 
-    for (const initialSource of initialSources) {
-      changes.set(initialSource, target);
+    // The original analysis is derived from others (and possibly from itself)
+    // The original analysis should no longer be mapped to its sources
+    // because it has been replaced by target
+    sources.delete(origin);
+    // Its origins are now the origins of the target
+    for (const source of originSources) {
+      // The sources of the original analysis are mapped to the target
+      changes.set(source, target);
+      // The target acquires its sources
+      targetSources.add(source);
     }
   }
 }
