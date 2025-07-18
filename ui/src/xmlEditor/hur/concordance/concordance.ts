c9bd5c0b42b36222e51e0f66e69ca277b4bc7931
Innokentii Smirnov <innokenty.smirnof@gmail.com>
2025-07-19 01:01:58 +0300

    Add concordance module

---
+++ ui/src/xmlEditor/hur/concordance/concordance.ts
@@ -0,0 +1,42 @@
+import { convertDictionary, updateDictionary } from '../common/utility';
+import { add } from '../common/utils';
+
+const sep = ',';
+
+export class Attestation {
+  text: string;
+  line: string;
+  constructor(text: string, line: string) {
+    this.text = text;
+    this.line = line;
+  }
+  toString(): string {
+    return this.text + sep + this.line;
+  }
+}
+
+const concordance = new Map<string, Set<string>>();
+
+export function addAttestation(analysis: string, attestation: Attestation) {
+  add(concordance, analysis, attestation.toString());
+}
+
+export function getAttestations(analysis: string): Attestation[] {
+  const current = concordance.get(analysis);
+  if (current === undefined) {
+    return [];
+  } else {
+    return Array.from(current).map((repr: string) => {
+      const [text, line] = repr.split(sep);
+      return new Attestation(text, line);
+    });
+  }
+}
+
+export function getConcordance(): { [key: string]: string[] } {
+  return convertDictionary(concordance);
+}
+
+export function updateConcordance(object: { [key: string]: string[] }) {
+  updateDictionary(concordance, object);
+}
