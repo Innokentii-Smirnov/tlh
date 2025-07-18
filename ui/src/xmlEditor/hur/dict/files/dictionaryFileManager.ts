b5e29195520254251653dd7a46b1c6c8fb689c6d
Innokentii Smirnov <innokenty.smirnof@gmail.com>
2025-07-19 01:03:51 +0300

    Add concordance (de)serialization

--- ui/src/xmlEditor/hur/dict/files/dictionaryFileManager.ts
+++ ui/src/xmlEditor/hur/dict/files/dictionaryFileManager.ts
@@ -1,13 +1,15 @@
 import { getDictionary, upgradeDictionary } from '../dictionary';
 import { getGlosses, upgradeGlosses } from '../../translations/glossProvider';
 import { getPartsOfSpeech, setPartsOfSpeech } from '../../partsOfSpeech/partsOfSpeech';
+import { getConcordance, updateConcordance } from '../../concordance/concordance';
 import { makeDownload } from '../../../../downloadHelper';
 
 export function downloadDictionary() {
   const dictionary = getDictionary();
   const glosses = getGlosses();
   const partsOfSpeech = getPartsOfSpeech();
-  const obj = {partsOfSpeech, dictionary, glosses};
+  const concordance = getConcordance();
+  const obj = {partsOfSpeech, dictionary, glosses, concordance};
   const jsonText = JSON.stringify(obj, undefined, '\t');
   makeDownload(jsonText, 'Dictionary.json');
 }
@@ -20,5 +22,8 @@ export async function readDict(file: File) {
   upgradeGlosses(glosses);
   if ('partsOfSpeech' in parsed) {
     setPartsOfSpeech(parsed.partsOfSpeech);
+  }
+  if ('concordance' in parsed) {
+    updateConcordance(parsed.concordance);
   }
 }
