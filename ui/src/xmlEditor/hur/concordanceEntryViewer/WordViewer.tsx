55e5dc5202c56663f80a6eba7ee3d9510d2b56c0
Innokentii Smirnov <innokenty.smirnof@gmail.com>
2025-07-21 02:46:02 +0300

    Add formatting to the transliterations

--- ui/src/xmlEditor/hur/concordanceEntryViewer/WordViewer.tsx
+++ ui/src/xmlEditor/hur/concordanceEntryViewer/WordViewer.tsx
@@ -1,5 +1,8 @@
 import { JSX } from 'react';
 import { Word } from '../corpus/wordConstructor';
+import { MyLeft, parseNewXml, XmlElementNode } from 'simple_xml';
+import { tlhXmlEditorConfig } from '../../tlhXmlEditorConfig';
+import { NodeDisplay } from '../../NodeDisplay';
 
 interface IProps {
   word: Word;
@@ -7,11 +10,14 @@ interface IProps {
 
 export function WordViewer({ word }: IProps): JSX.Element {
   const { transliteration, segmentation, gloss } = word;
+  const parseResult = parseNewXml('<w>' + transliteration + '</w>', tlhXmlEditorConfig.readConfig);
   
   return (
     <div className="corpus-word">
       <div className="corpus-word-field">
-        {transliteration}
+        {parseResult instanceof MyLeft ? <>{transliteration}</>
+        : <NodeDisplay node={parseResult.value as XmlElementNode}
+                       isLeftSide={true} />}
       </div>
       <div className="corpus-word-field">
         {segmentation}
