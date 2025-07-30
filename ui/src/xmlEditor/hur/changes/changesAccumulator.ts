5705b97f1b77ad6fa9de1328f8418d4ff8562bfd
Innokentii Smirnov <innokenty.smirnof@gmail.com>
2025-07-30 12:10:17 +0300

    Remove undone changes from the changes list

--- ui/src/xmlEditor/hur/changes/changesAccumulator.ts
+++ ui/src/xmlEditor/hur/changes/changesAccumulator.ts
@@ -5,6 +5,15 @@ import { writeMorphAnalysisValue } from '../../../model/morphologicalAnalysis';
 const changes = new Map<string, string>();
 const sources = new Map<string, Set<string>>();
 
+function addChangeWithIdentityCheck(origin: string, target: string): void {
+  if (target !== origin) {
+    changes.set(origin, target);
+  } else {
+    // Do not store identity pairs in the changes list
+    changes.delete(origin);
+  }
+}
+
 export function addChange(origin: string, target: string, targetIsExtant: boolean): void {
   // Stop if no actual change
   if (target === origin) {
@@ -24,7 +33,7 @@ export function addChange(origin: string, target: string, targetIsExtant: boolea
   const originSources = sources.get(origin);
   if (originSources === undefined) {
     // The original analysis is underived
-    changes.set(origin, target);
+    addChangeWithIdentityCheck(origin, target);
     // The target acquires an additional source
     targetSources.add(origin);
   } else {
@@ -35,7 +44,7 @@ export function addChange(origin: string, target: string, targetIsExtant: boolea
     // Its origins are now the origins of the target
     for (const source of originSources) {
       // The sources of the original analysis are mapped to the target
-      changes.set(source, target);
+      addChangeWithIdentityCheck(source, target);
       // The target acquires its sources
       targetSources.add(source);
     }
