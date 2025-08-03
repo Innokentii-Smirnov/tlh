21e560c5e1e1a33371c7b75d604d9b29b9a58352
Innokentii Smirnov <innokenty.smirnof@gmail.com>
2025-08-03 18:31:44 +0300

    Add run command and change multiline strings quoting

--- HurrianLexicalDatabaseContainer/getHurrianDictionary.php
+++ HurrianLexicalDatabaseContainer/getHurrianDictionary.php
@@ -1,16 +1,15 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: text/json');
+header('Content-Type: application/json');
 $dbFileName = 'hurrianLexicalDatabase.sqlite';
 if (file_exists($dbFileName))
 {
   $db = new SQLite3($dbFileName);
-  $sql = <<<SQL
-  SELECT w.transcription, w.segmentation, l.translation_de, s.morph_tag, l.part_of_speech, l.det
-  FROM wordforms w
-    INNER JOIN lemmata l ON l.lemma_id = w.lemma_id
-    INNER JOIN suffix_chains s ON s.suffix_chain_id = w.suffix_chain_id;
-  SQL;
+  $sql = 
+  'SELECT w.transcription, w.segmentation, l.translation_de, s.morph_tag, l.part_of_speech, l.det '.
+  'FROM wordforms w '.
+    'INNER JOIN lemmata l ON l.lemma_id = w.lemma_id '.
+    'INNER JOIN suffix_chains s ON s.suffix_chain_id = w.suffix_chain_id';
   $result = $db->query($sql);
   $data = array();
   while ($row = $result->fetchArray())
---
+++ HurrianLexicalDatabaseContainer/run.bat
@@ -0,0 +1,1 @@
+php -d extension=sqlite3 -S 127.0.0.2:8067
