713758af99ebde600401a21b41b45146518133a1
Innokentii Smirnov <innokenty.smirnof@gmail.com>
2025-08-04 13:02:21 +0300

    Fix the alphabetisation of g with dot

--- ui/src/xmlEditor/hur/common/comparison.ts
+++ ui/src/xmlEditor/hur/common/comparison.ts
@@ -1,2 +1,22 @@
 const lang = 'lv';
-export const compare = new Intl.Collator(lang).compare;
+const latvianCompare = new Intl.Collator(lang).compare;
+function compareLetter(a: string, b: string): number {
+  if (a === 'g' && b === 'ġ') {
+    return -1;
+  } else if (a === 'ġ' && b === 'g') {
+    return 1;
+  } else {
+    return latvianCompare(a, b);
+  }
+}
+export function compare(a: string, b: string): number {
+  for (let i = 0; i < Math.min(a.length, b.length); i++) {
+    const comparisonResult = compareLetter(a[i], b[i]);
+    if (comparisonResult === 0) {
+      continue;
+    } else {
+      return comparisonResult;
+    }
+  }
+  return b.length - a.length;
+}
