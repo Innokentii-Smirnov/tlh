63ec69d40308e018ba7c224c15b0c8c8e8bce6b1
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-09-22 17:38:22 +0300

    Remove initial messages (submodule)

--- ui/src/xmlEditor/hur/lexicalDatabaseInteraction.ts
+++ ui/src/xmlEditor/hur/lexicalDatabaseInteraction.ts
@@ -7,19 +7,17 @@ import { replaceMorphologicalAnalysis } from './corpus/corpus';
 export const updatesStream = new EventSource(getHurrianLexicalDatabaseUpdatesUrl);
 export function enableLexicalDatabaseUpdateHandling(): void {
   const replaceMorphologicalAnalysisListener = (event: MessageEvent) => {
-    if (event.data !== 'Starting' && event.data !== '"Initial message"') {
-      const { transcriptions, origin, target } = JSON.parse(event.data);
-      const newMorphologicalAnalysis = readMorphAnalysisValue(target);
-      if (newMorphologicalAnalysis !== undefined) {
-        const newDictionary = modifyAnalysis(dictionary, transcriptions,
-                                             origin, newMorphologicalAnalysis);
-        setGlobalDictionary(newDictionary);
-      }
-      // The corpus should be updated before the concordance
-      // Since the old analysis is used to find the lines to update
-      replaceMorphologicalAnalysis(origin, target);
-      updateConcordanceKey(origin, target);
+    const { transcriptions, origin, target } = JSON.parse(event.data);
+    const newMorphologicalAnalysis = readMorphAnalysisValue(target);
+    if (newMorphologicalAnalysis !== undefined) {
+      const newDictionary = modifyAnalysis(dictionary, transcriptions,
+                                            origin, newMorphologicalAnalysis);
+      setGlobalDictionary(newDictionary);
     }
+    // The corpus should be updated before the concordance
+    // Since the old analysis is used to find the lines to update
+    replaceMorphologicalAnalysis(origin, target);
+    updateConcordanceKey(origin, target);
   };
   updatesStream.addEventListener('replaceMorphologicalAnalysis',
                                  replaceMorphologicalAnalysisListener);
