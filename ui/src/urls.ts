765e4737a80ca08b4eac39d1fdd3f12fb9e4b544
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-10-25 16:43:04 +0300

    Change the hur. lex. DB URL

--- ui/src/urls.ts
+++ ui/src/urls.ts
@@ -56,7 +56,7 @@ export const secondXmlReviewUrl = 'secondXmlReview';
 export const approveDocumentUrl = 'approveDocument';
 
 const hurrianLexicalDatabaseUrl = process.env.NODE_ENV !== 'development'
-  ? 'http://hurrian_lexical_database:8080'
+  ? '/hurlexdb'
   : 'http://localhost:8080';
 
 export const getHurrianLexicalDatabaseUrl = hurrianLexicalDatabaseUrl;
