ce5edd21e484f7af22ac6c8728539dce02bcb2fd
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-11-28 12:20:42 +0300

    Remove extra semicolon in the query

--- html/model/MorphologicalAnalysis.php
+++ html/model/MorphologicalAnalysis.php
@@ -38,7 +38,7 @@ class MorphologicalAnalysis
       inner join tive_stems as stem
         on analysis.stem_id = stem.stem_id
       inner join tive_suffix_chains as suff
-        on analysis.suffix_chain_id = suff.suffix_chain_id;
+        on analysis.suffix_chain_id = suff.suffix_chain_id
     where analysis.stem_id = ?;
 SQL;
     return SqlHelpers::executeMultiSelectQuery(
