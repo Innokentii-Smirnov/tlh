e5b1f863eacf6045d119d40b356710283404ab19
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-11-29 10:45:36 +0300

    Add tables for word-forms and morphosyntactic words

--- sql_inits/30_tive.sql
+++ sql_inits/30_tive.sql
@@ -34,3 +34,23 @@ create table if not exists tive_morphological_analyses (
   constraint fk_morph_suffix_chain_id foreign key (suffix_chain_id)
     references tive_suffix_chains (suffix_chain_id)
 );
+
+create table if not exists tive_wordforms (
+  wordform_id   smallint unsigned auto_increment  not null,
+  transcription varchar(127)                      not null,
+  constraint pk_tive_wordforms primary key (wordform_id)
+);
+
+-- A morphosyntactic word is a word-form with a particular
+-- set of morphosyntactic properties (case, number, etc).
+
+create table if not exists tive_morphosyntactic_words (
+  morphosyntactic_word_id   smallint unsigned auto_increment  not null,
+  wordform_id               smallint unsigned                 not null,
+  morphological_analysis_id smallint unsigned                 not null,
+  constraint pk_tive_morphosyntactis_words  primary key (morphosyntactic_word_id),
+  constraint fk_morphosynt_word_wordform_id foreign key (wordform_id)
+    references tive_wordforms (wordform_id),
+  constraint fk_morphosynt_word_morph_id    foreign key (morphological_analysis_id)
+    references tive_morphological_analyses (morphological_analysis_id)
+);
