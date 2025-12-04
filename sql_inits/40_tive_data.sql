8d60afc2d6f0bdb77b5d874ac4d1ff86a60bb478
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-12-04 20:40:20 +0300

    Further optimize Hurrian lexical database population

--- sql_inits/40_tive_data.sql
+++ sql_inits/40_tive_data.sql
@@ -47,119 +47,24 @@ load data
 
 -- Hurrian morphosyntactic words
 
-insert into tive_morphosyntactic_words (wordform_id, morphological_analysis_id)
-select wordform_id, morphological_analysis_id from
-  (select wordform_id from tive_wordforms
-    where transcription = 'nāli') wordforms cross join
-  (select morphological_analysis_id from tive_morphological_analyses ma
-    inner join tive_stems stem on ma.stem_id = stem.stem_id
-    inner join tive_suffix_chains suff on ma.suffix_chain_id = suff.suffix_chain_id
-    where stem.deu = 'Rehbock' and suff.morph_tag = '.ABS') morphs;
+create temporary table tive_morphosyntactic_words_input (
+  transcription varchar(127) character set utf8mb4 collate utf8mb4_bin                        not null,
+  deu       varchar(127)                                                                      not null,
+  morph_tag varchar(127)                                                                      not null,
+  pos enum ('ADV', 'CONJ', 'PREP', 'POST', 'INTJ','NUM', 'PRON','NF','noun','verb','unclear') not null
+);
+
+load data
+  local
+  infile '/hur_lex_data/morphosyntactic_words.tsv'
+  into table tive_morphosyntactic_words_input
+  (transcription, deu, morph_tag, pos);
 
 insert into tive_morphosyntactic_words (wordform_id, morphological_analysis_id)
-select wordform_id, morphological_analysis_id from
-  (select wordform_id from tive_wordforms
-    where transcription = 'melaḫḫum') wordforms cross join
-  (select morphological_analysis_id from tive_morphological_analyses ma
-    inner join tive_stems stem on ma.stem_id = stem.stem_id
-    inner join tive_suffix_chains suff on ma.suffix_chain_id = suff.suffix_chain_id
-    where stem.deu = 'vertreiben' and suff.morph_tag = 'TR.PFV-3A.SG') morphs;
-
-insert into tive_morphosyntactic_words (wordform_id, morphological_analysis_id)
-select wordform_id, morphological_analysis_id from
-  (select wordform_id from tive_wordforms
-    where transcription regexp 'p[aā]banni$') wordforms cross join
-  (select morphological_analysis_id from tive_morphological_analyses ma
-    inner join tive_stems stem on ma.stem_id = stem.stem_id
-    inner join tive_suffix_chains suff on ma.suffix_chain_id = suff.suffix_chain_id
-    where stem.deu = 'Berg' and suff.morph_tag in ('.ABS', 'RElAT.SG-DIR/LOC')) morphs;
-
-insert into tive_morphosyntactic_words (wordform_id, morphological_analysis_id)
-select wordform_id, morphological_analysis_id from
-  (select wordform_id from tive_wordforms
-    where transcription = 'fūru') wordforms cross join
-  (select morphological_analysis_id from tive_morphological_analyses ma
-    inner join tive_stems stem on ma.stem_id = stem.stem_id
-    inner join tive_suffix_chains suff on ma.suffix_chain_id = suff.suffix_chain_id
-    where stem.deu = 'sehen' and suff.morph_tag = 'MED') morphs;
-
-insert into tive_morphosyntactic_words (wordform_id, morphological_analysis_id)
-select wordform_id, morphological_analysis_id from
-  (select wordform_id from tive_wordforms
-    where transcription = 'šidarillōm') wordforms cross join
-  (select morphological_analysis_id from tive_morphological_analyses ma
-    inner join tive_stems stem on ma.stem_id = stem.stem_id
-    inner join tive_suffix_chains suff on ma.suffix_chain_id = suff.suffix_chain_id
-    where stem.deu = 'verfluchen' and suff.morph_tag = 'ITER-INGR-TR.PFV-3A.SG') morphs;
-
-insert into tive_morphosyntactic_words (wordform_id, morphological_analysis_id)
-select wordform_id, morphological_analysis_id from
-  (select wordform_id from tive_wordforms
-    where transcription = 'idilānni') wordforms cross join
-  (select morphological_analysis_id from tive_morphological_analyses ma
-    inner join tive_stems stem on ma.stem_id = stem.stem_id
-    inner join tive_suffix_chains suff on ma.suffix_chain_id = suff.suffix_chain_id
-    where stem.deu = 'schlagen' and suff.morph_tag = 'MOD.ACT-l-DESID') morphs;
-
-insert into tive_morphosyntactic_words (wordform_id, morphological_analysis_id)
-select wordform_id, morphological_analysis_id from
-  (select wordform_id from tive_wordforms
-    where transcription = 'amelānni') wordforms cross join
-  (select morphological_analysis_id from tive_morphological_analyses ma
-    inner join tive_stems stem on ma.stem_id = stem.stem_id
-    inner join tive_suffix_chains suff on ma.suffix_chain_id = suff.suffix_chain_id
-    where stem.deu = 'verbrennen' and suff.morph_tag = 'MOD.ACT-l-DESID') morphs;
-
-insert into tive_morphosyntactic_words (wordform_id, morphological_analysis_id)
-select wordform_id, morphological_analysis_id from
-  (select wordform_id from tive_wordforms
-    where transcription regexp 'tārr[ei]ž') wordforms cross join
-  (select morphological_analysis_id from tive_morphological_analyses ma
-    inner join tive_stems stem on ma.stem_id = stem.stem_id
-    inner join tive_suffix_chains suff on ma.suffix_chain_id = suff.suffix_chain_id
-    where stem.deu = 'Feuer' and suff.morph_tag = 'RELAT.SG-ERG') morphs;
-
-insert into tive_morphosyntactic_words (wordform_id, morphological_analysis_id)
-select wordform_id, morphological_analysis_id from
-  (select wordform_id from tive_wordforms
-    where transcription regexp 'p[aā]banniž') wordforms cross join
-  (select morphological_analysis_id from tive_morphological_analyses ma
-    inner join tive_stems stem on ma.stem_id = stem.stem_id
-    inner join tive_suffix_chains suff on ma.suffix_chain_id = suff.suffix_chain_id
-    where stem.deu = 'Berg' and suff.morph_tag = 'RELAT.SG-ERG') morphs;
-
-insert into tive_morphosyntactic_words (wordform_id, morphological_analysis_id)
-select wordform_id, morphological_analysis_id from
-  (select wordform_id from tive_wordforms
-    where transcription = 'kulōrum') wordforms cross join
-  (select morphological_analysis_id from tive_morphological_analyses ma
-    inner join tive_stems stem on ma.stem_id = stem.stem_id
-    inner join tive_suffix_chains suff on ma.suffix_chain_id = suff.suffix_chain_id
-    where stem.deu = 'sprechen' and suff.morph_tag = 'TR.PFV-3A.SG') morphs;
-
-insert into tive_morphosyntactic_words (wordform_id, morphological_analysis_id)
-select wordform_id, morphological_analysis_id from
-  (select wordform_id from tive_wordforms
-    where transcription = 'amarillōm') wordforms cross join
-  (select morphological_analysis_id from tive_morphological_analyses ma
-    inner join tive_stems stem on ma.stem_id = stem.stem_id
-    inner join tive_suffix_chains suff on ma.suffix_chain_id = suff.suffix_chain_id
-    where stem.deu = 'ansehen' and suff.morph_tag = 'ITER-INGR-TR.PFV-3A.SG') morphs;
-
-insert into tive_morphosyntactic_words (wordform_id, morphological_analysis_id)
-select wordform_id, morphological_analysis_id from
-  (select wordform_id from tive_wordforms
-    where transcription regexp '[aā]ima') wordforms cross join
-  (select morphological_analysis_id from tive_morphological_analyses ma
-    inner join tive_stems stem on ma.stem_id = stem.stem_id
-    inner join tive_suffix_chains suff on ma.suffix_chain_id = suff.suffix_chain_id
-    where stem.deu = 'falls; wenn' and suff.morph_tag = '=CON') morphs;
-
-insert into tive_morphosyntactic_words (wordform_id, morphological_analysis_id)
-select wordform_id, morphological_analysis_id from
-  (select wordform_id from tive_wordforms
-    where transcription regexp '^[aā]i$') wordforms cross join
-  (select morphological_analysis_id from tive_morphological_analyses ma
-    inner join tive_stems stem on ma.stem_id = stem.stem_id
-    inner join tive_suffix_chains suff on ma.suffix_chain_id = suff.suffix_chain_id
-    where stem.deu = 'falls; wenn' and suff.morph_tag = '') morphs;
+select w.wordform_id, ma.morphological_analysis_id
+from tive_wordforms w
+  cross join tive_morphological_analyses ma
+  inner join tive_stems stem on ma.stem_id = stem.stem_id
+  inner join tive_suffix_chains suff on ma.suffix_chain_id = suff.suffix_chain_id
+  inner join tive_morphosyntactic_words_input input on w.transcription regexp input.transcription
+    and stem.deu = input.deu and suff.morph_tag = input.morph_tag and suff.pos = input.pos;
