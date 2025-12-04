359a969c8456283da4289b85e3fde563073d988f
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-12-04 22:51:24 +0300

    Do not remove whitespace from the queries

--- tive_graphql_test/run_mutations.sh
+++ tive_graphql_test/run_mutations.sh
@@ -1,6 +1,6 @@
 rm -r mutation_output
 mkdir mutation_output
 for file in mutations/*; do
-  data=$(tr -d '[:space:]' < $file | jq -R '{query: .}')
+  data=$(jq -R --slurp '{query: .}' $file)
   ./query.sh "$data" > "mutation_output/$(basename $file .graphql).json"
 done
--- tive_graphql_test/test.sh
+++ tive_graphql_test/test.sh
@@ -1,7 +1,7 @@
 rm -r output
 mkdir output
 for file in queries/*; do
-  data=$(tr -d '[:space:]' < $file | jq -R '{query: .}')
+  data=$(jq -R --slurp '{query: .}' $file)
   ./query.sh "$data" > "output/$(basename $file .graphql).json"
 done
 ./check.sh
