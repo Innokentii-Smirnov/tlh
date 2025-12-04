96c07a307d8a7e2e5ffba33464f4fac6dda575e6
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-12-04 19:31:52 +0300

    Optimize Hurrian lexical database population

--- sql_inits/40_tive_data.sql
+++ sql_inits/40_tive_data.sql
@@ -2,242 +2,48 @@ use hpm;
 
 -- Hurrian stems
 
-insert into tive_stems (form, pos, deu, eng)
-values ('nāli', 'noun', 'Rehbock', 'deer');
-
-insert into tive_stems (form, pos, deu, eng)
-values ('mel+aḫḫ', 'verb', 'vertreiben', 'drive away');
-
-insert into tive_stems (form, pos, deu, eng)
-values ('paba+ni', 'noun', 'Berg', 'mountain');
-
-insert into tive_stems (form, pos, deu, eng)
-values ('fūr', 'verb', 'sehen', 'see');
-
-insert into tive_stems (form, pos, deu, eng)
-values ('šid', 'verb', 'verfluchen', 'curse');
-
-insert into tive_stems (form, pos, deu, eng)
-values ('id', 'verb', 'schlagen', 'strike');
-
-insert into tive_stems (form, pos, deu, eng)
-values ('am', 'verb', 'verbrennen', 'burn');
-
-insert into tive_stems (form, pos, deu, eng)
-values ('tāri', 'noun', 'Feuer', 'fire');
-
-insert into tive_stems (form, pos, deu, eng)
-values ('kul', 'verb', 'sprechen', 'speak');
-
-insert into tive_stems (form, pos, deu, eng)
-values ('am', 'verb', 'ansehen', 'look at');
-
-insert into tive_stems (form, pos, deu, eng)
-values ('ai', 'CONJ', 'falls; wenn', 'in case; if');
+load data
+  local
+  infile '/hur_lex_data/stems.tsv'
+  into table tive_stems
+  (form, pos, deu, eng);
 
 -- Hurrian suffix chains
 
-insert into tive_suffix_chains (suffixes, morph_tag, pos)
-values ('', '.ABS', 'noun');
-
-insert into tive_suffix_chains (suffixes, morph_tag, pos)
-values ('ne-ž', 'RELAT.SG-ERG', 'noun');
-
-insert into tive_suffix_chains (suffixes, morph_tag, pos)
-values ('o-m', 'TR.PFV-3A.SG', 'verb');
-
-insert into tive_suffix_chains (suffixes, morph_tag, pos)
-values ('ne-e', 'RELAT.SG-DIR/LOC', 'noun');
-
-insert into tive_suffix_chains (suffixes, morph_tag, pos)
-values ('u', 'MED', 'verb');
-
-insert into tive_suffix_chains (suffixes, morph_tag, pos)
-values ('ar-ill-ō-m', 'ITER-INGR-TR.PFV-3A.SG', 'verb');
-
-insert into tive_suffix_chains (suffixes, morph_tag, pos)
-values ('i-l-ānni', 'MOD.ACT-l-DESID', 'verb');
-
-insert into tive_suffix_chains (suffixes, morph_tag, pos)
-values ('ōr-o-m', 'ōr-TR.PFV-3A.SG', 'verb');
-
-insert into tive_suffix_chains (suffixes, morph_tag, pos)
-values ('ār-a', 'ITER-3A.SG', 'verb');
-
-insert into tive_suffix_chains (suffixes, morph_tag, pos)
-values ('=ma', '=CON', 'CONJ');
-
-insert into tive_suffix_chains (suffixes, morph_tag, pos)
-values ('', '', 'CONJ');
+load data
+  local
+  infile '/hur_lex_data/suffix_chains.tsv'
+  into table tive_suffix_chains
+  (suffixes, morph_tag, pos);
 
 -- Hurrian morphological analyses
 
-insert into tive_morphological_analyses (stem_id, suffix_chain_id)
-values (
-  (select stem_id from tive_stems where deu = 'Rehbock'),
-  (select suffix_chain_id from tive_suffix_chains where morph_tag = '.ABS')
+create temporary table tive_morphological_analyses_input (
+  deu       varchar(127)                                                                      not null,
+  morph_tag varchar(127)                                                                      not null,
+  pos enum ('ADV', 'CONJ', 'PREP', 'POST', 'INTJ','NUM', 'PRON','NF','noun','verb','unclear') not null
 );
 
-insert into tive_morphological_analyses (stem_id, suffix_chain_id)
-values (
-  (select stem_id from tive_stems where deu = 'Berg'),
-  (select suffix_chain_id from tive_suffix_chains where morph_tag = 'RELAT.SG-ERG')
-);
+load data
+  local
+  infile '/hur_lex_data/morphological_analyses.tsv'
+  into table tive_morphological_analyses_input
+  (deu, morph_tag, pos);
 
 insert into tive_morphological_analyses (stem_id, suffix_chain_id)
-values (
-  (select stem_id from tive_stems where deu = 'vertreiben'),
-  (select suffix_chain_id from tive_suffix_chains where morph_tag = 'TR.PFV-3A.SG')
-);
-
-insert into tive_morphological_analyses (stem_id, suffix_chain_id)
-values (
-  (select stem_id from tive_stems where deu = 'Berg'),
-  (select suffix_chain_id from tive_suffix_chains where morph_tag = 'RElAT.SG-DIR/LOC')
-);
-
-insert into tive_morphological_analyses (stem_id, suffix_chain_id)
-values (
-  (select stem_id from tive_stems where deu = 'sehen'),
-  (select suffix_chain_id from tive_suffix_chains where morph_tag = 'MED')
-);
-
-insert into tive_morphological_analyses (stem_id, suffix_chain_id)
-values (
-  (select stem_id from tive_stems where deu = 'verfluchen'),
-  (select suffix_chain_id from tive_suffix_chains where morph_tag = 'ITER-INGR-TR.PFV-3A.SG')
-);
-
-insert into tive_morphological_analyses (stem_id, suffix_chain_id)
-values (
-  (select stem_id from tive_stems where deu = 'Berg'),
-  (select suffix_chain_id from tive_suffix_chains where morph_tag = '.ABS')
-);
-
-insert into tive_morphological_analyses (stem_id, suffix_chain_id)
-values (
-  (select stem_id from tive_stems where deu = 'verbrennen'),
-  (select suffix_chain_id from tive_suffix_chains where morph_tag = 'MOD.ACT-l-DESID')
-);
-
-insert into tive_morphological_analyses (stem_id, suffix_chain_id)
-values (
-  (select stem_id from tive_stems where deu = 'Feuer'),
-  (select suffix_chain_id from tive_suffix_chains where morph_tag = 'RELAT.SG-ERG')
-);
-
-insert into tive_morphological_analyses (stem_id, suffix_chain_id)
-values (
-  (select stem_id from tive_stems where deu = 'schlagen'),
-  (select suffix_chain_id from tive_suffix_chains where morph_tag = 'MOD.ACT-l-DESID')
-);
-
-insert into tive_morphological_analyses (stem_id, suffix_chain_id)
-values (
-  (select stem_id from tive_stems where deu = 'sprechen'),
-  (select suffix_chain_id from tive_suffix_chains where morph_tag = 'ōr-TR.PFV-3A.SG')
-);
-
-insert into tive_morphological_analyses (stem_id, suffix_chain_id)
-values (
-  (select stem_id from tive_stems where deu = 'verfluchen'),
-  (select suffix_chain_id from tive_suffix_chains where morph_tag = 'ITER-3A.SG')
-);
-
-insert into tive_morphological_analyses (stem_id, suffix_chain_id)
-values (
-  (select stem_id from tive_stems where deu = 'ansehen'),
-  (select suffix_chain_id from tive_suffix_chains where morph_tag = 'ITER-INGR-TR.PFV-3A.SG')
-);
-
-insert into tive_morphological_analyses (stem_id, suffix_chain_id)
-values (
-  (select stem_id from tive_stems where deu = 'falls; wenn'),
-  (select suffix_chain_id from tive_suffix_chains where morph_tag = '=CON')
-);
-
-insert into tive_morphological_analyses (stem_id, suffix_chain_id)
-values (
-  (select stem_id from tive_stems where deu = 'falls; wenn'),
-  (select suffix_chain_id from tive_suffix_chains where morph_tag = '' and pos = 'CONJ')
-);
+select
+  (select stem_id from tive_stems where deu = input.deu),
+  (select suffix_chain_id from tive_suffix_chains
+   where morph_tag = input.morph_tag and pos = input.pos)
+from tive_morphological_analyses_input input;
 
 -- Hurrian word-forms
 
-insert into tive_wordforms (transcription)
-values ('nāli');
-
-insert into tive_wordforms (transcription)
-values ('ide[v]inež');
-
-insert into tive_wordforms (transcription)
-values ('pabanniž');
-
-insert into tive_wordforms (transcription)
-values ('melaḫḫum');
-
-insert into tive_wordforms (transcription)
-values ('ōlbīni');
-
-insert into tive_wordforms (transcription)
-values ('pabanni');
-
-insert into tive_wordforms (transcription)
-values ('ḫabānab');
-
-insert into tive_wordforms (transcription)
-values ('fūru');
-
-insert into tive_wordforms (transcription)
-values ('tēlu');
-
-insert into tive_wordforms (transcription)
-values ('tapšū');
-
-insert into tive_wordforms (transcription)
-values ('šidarillōm');
-
-insert into tive_wordforms (transcription)
-values ('āi');
-
-insert into tive_wordforms (transcription)
-values ('pābanni');
-
-insert into tive_wordforms (transcription)
-values ('amelānni');
-
-insert into tive_wordforms (transcription)
-values ('tārrež');
-
-insert into tive_wordforms (transcription)
-values ('idilānni');
-
-insert into tive_wordforms (transcription)
-values ('teššōbaž');
-
-insert into tive_wordforms (transcription)
-values ('tārriž');
-
-insert into tive_wordforms (transcription)
-values ('pabāni');
-
-insert into tive_wordforms (transcription)
-values ('ḫāžīmai');
-
-insert into tive_wordforms (transcription)
-values ('kulōrum');
-
-insert into tive_wordforms (transcription)
-values ('šidāra');
-
-insert into tive_wordforms (transcription)
-values ('amarillōm');
-
-insert into tive_wordforms (transcription)
-values ('aima');
-
-insert into tive_wordforms (transcription)
-values ('āima');
+load data
+  local
+  infile '/hur_lex_data/transcriptions.tsv'
+  into table tive_wordforms
+  (transcription);
 
 -- Hurrian morphosyntactic words
 
