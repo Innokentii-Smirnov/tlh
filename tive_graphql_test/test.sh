08b8d6daf1900a196a70716577fa0b6b6e7c8460
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-12-05 21:05:16 +0300

    Only use .graphql files for the test

--- tive_graphql_test/test.sh
+++ tive_graphql_test/test.sh
@@ -1,6 +1,6 @@
 rm -r "output/$1"
 mkdir -p "output/$1"
-for file in input/"$1"/*; do
+for file in input/"$1"/*.graphql; do
   data=$(jq -R --slurp '{query: .}' $file)
   ./query.sh "$data" > "output/$1/$(basename $file .graphql).json"
 done
